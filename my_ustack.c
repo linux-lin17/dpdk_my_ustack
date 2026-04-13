bfa6d606ffc620bdd308fd2bad30845ac65c1524
linux-lin17 <17yy.lincs@gmail.com>
2026-04-13 13:32:22 +0000

    TCP : send

--- my_ustack.c
+++ my_ustack.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <rte_eal.h>
 #include <rte_ethdev.h>
-#include <rte_mempool.h>
 #include <arpa/inet.h>
 
 
@@ -9,6 +8,7 @@
 #define BURST_SIZE      128
 
 #define ENABLE_SEND     1
+#define ENABLE_TCP		1
 
 uint8_t global_smac[RTE_ETHER_ADDR_LEN];
 uint8_t global_dmac[RTE_ETHER_ADDR_LEN];
@@ -21,10 +21,39 @@ uint16_t global_dport;
 
 int global_portid = 0;
 
+#if ENABLE_TCP
+
+uint8_t global_flags;
+uint32_t global_seqnum;
+uint32_t global_acknum;
+
+typedef enum __USTACK_TCP_STATUS {
+
+	USTACK_TCP_STATUS_CLOSED = 0,
+	USTACK_TCP_STATUS_LISTEN,
+	USTACK_TCP_STATUS_SYN_RCVD,
+	USTACK_TCP_STATUS_SYN_SENT,
+	USTACK_TCP_STATUS_ESTABLISHED,
+	USTACK_TCP_STATUS_FIN_WAIT_1,
+	USTACK_TCP_STATUS_FIN_WAIT_2,
+	USTACK_TCP_STATUS_CLOSING,
+	USTACK_TCP_STATUS_TIMEWAIT,
+	USTACK_TCP_STATUS_CLOSE_WAIT,
+	USTACK_TCP_STATUS_LAST_ACK
+	
+} USTACK_TCP_STATUS;
+
+uint8_t tcp_status = USTACK_TCP_STATUS_LISTEN;
+
+
+#endif
+
 static const struct rte_eth_conf port_conf_default = {
 	.rxmode = { .max_rx_pkt_len = RTE_ETHER_MAX_LEN }
 };
 
+
+// 网卡初始化
 static int ustack_init_port(struct rte_mempool *mbuf_pool) {
 
     uint16_t nb_sys_ports = rte_eth_dev_count_avail();
@@ -107,6 +136,49 @@ static int ustack_encode_udp_pkt(uint8_t *msg, uint8_t *data, uint16_t total_len
 	return 0;
 }
 
+static int ustack_encode_tcp_pkt(uint8_t *msg, uint16_t total_len) {
+
+	//1 ether header
+
+	struct rte_ether_hdr *eth = (struct rte_ether_hdr *)msg;
+	rte_memcpy(eth->d_addr.addr_bytes, global_dmac, RTE_ETHER_ADDR_LEN);
+	rte_memcpy(eth->s_addr.addr_bytes, global_smac, RTE_ETHER_ADDR_LEN);
+	eth->ether_type = htons(RTE_ETHER_TYPE_IPV4);
+
+	//1 ip header
+	struct rte_ipv4_hdr *ip = (struct rte_ipv4_hdr*)(eth + 1); //msg + sizeof(struct rte_ether_hdr);
+	ip->version_ihl = 0x45;
+	ip->type_of_service = 0;
+	ip->total_length = htons(total_len - sizeof(struct rte_ether_hdr));
+	ip->packet_id = 0;
+	ip->fragment_offset = 0;
+	ip->time_to_live = 64;
+	ip->next_proto_id = IPPROTO_TCP;
+	ip->src_addr = global_sip;
+	ip->dst_addr = global_dip;
+
+	ip->hdr_checksum = 0;
+	ip->hdr_checksum = rte_ipv4_cksum(ip);
+
+	//1 tcp header
+
+	struct rte_tcp_hdr *tcp = (struct rte_tcp_hdr *)(ip + 1);
+	tcp->src_port = global_sport;
+	tcp->dst_port = global_dport;
+	tcp->sent_seq = htonl(12345);
+	tcp->recv_ack = htonl(global_seqnum + 1);
+	tcp->data_off = 0x50;
+	tcp->tcp_flags = RTE_TCP_SYN_FLAG | RTE_TCP_ACK_FLAG; // 0x1 << 1;
+	tcp->rx_win = htons(4096);
+
+	tcp->cksum = 0;
+	tcp->cksum = rte_ipv4_udptcp_cksum(ip, tcp);
+	
+	return 0;
+}
+
+
+
 int main(int argc, char *argv[]) {
     // 1. 初始化 DPDK 环境抽象层 (EAL)
     // 这是所有 DPDK 程序必须调用的第一步
@@ -115,7 +187,7 @@ int main(int argc, char *argv[]) {
         rte_exit(EXIT_FAILURE, "Error with EAL initialization\n");
     }
 
-    // 2. 你的业务代码以后就写在这里
+    // 2. 业务代码以后就写在这里
     printf("====== DPDK Environment Init Successful! ======\n");
 
     struct rte_mempool *mbuf_pool = rte_pktmbuf_pool_create("mbuf pool", NUM_MBUFS, 0, 0, RTE_MBUF_DEFAULT_BUF_SIZE, rte_socket_id());
@@ -157,7 +229,7 @@ int main(int argc, char *argv[]) {
 
 				struct in_addr addr;
 				addr.s_addr = iphdr->src_addr;
-				printf("sip %s:%d --> ", inet_ntoa(addr), ntohs(udphdr->src_port));
+				printf("udp sip %s:%d --> ", inet_ntoa(addr), ntohs(udphdr->src_port));
 
 				addr.s_addr = iphdr->dst_addr;
 				printf("dip %s:%d --> ", inet_ntoa(addr), ntohs(udphdr->dst_port));
@@ -181,9 +253,79 @@ int main(int argc, char *argv[]) {
 
 #endif
                 printf("udp : %s\n", (char *)(udphdr + 1));
-            }
+            } else if (iphdr->next_proto_id == IPPROTO_TCP){
 
-        }
+				struct rte_tcp_hdr *tcphdr = (struct rte_tcp_hdr *)(iphdr + 1);
+
+				rte_memcpy(global_smac, ethhdr->d_addr.addr_bytes, RTE_ETHER_ADDR_LEN);
+				rte_memcpy(global_dmac, ethhdr->s_addr.addr_bytes, RTE_ETHER_ADDR_LEN);            
+
+				rte_memcpy(&global_sip, &iphdr->dst_addr, sizeof(uint32_t));
+				rte_memcpy(&global_dip, &iphdr->src_addr, sizeof(uint32_t));    
+                
+				rte_memcpy(&global_sport, &tcphdr->dst_port, sizeof(uint16_t));
+				rte_memcpy(&global_dport, &tcphdr->src_port, sizeof(uint16_t));
+				
+				global_flags = tcphdr->tcp_flags;
+				global_seqnum = ntohl(tcphdr->sent_seq);
+				global_acknum = ntohl(tcphdr->recv_ack);
+
+				struct in_addr addr;
+				addr.s_addr = iphdr->src_addr;
+				printf("tcp pkt %s:%d --> ", inet_ntoa(addr), ntohs(tcphdr->src_port));
+
+				addr.s_addr = iphdr->dst_addr;
+				printf("dip %s:%d , flags: %x, seqnum: %d, acknum: %d\n", inet_ntoa(addr), ntohs(tcphdr->dst_port), 
+					global_flags, global_seqnum, global_acknum);
+
+				if (global_flags & RTE_TCP_SYN_FLAG) {
+
+					if (tcp_status == USTACK_TCP_STATUS_LISTEN) {
+						uint16_t total_len = sizeof(struct rte_tcp_hdr) + sizeof(struct rte_ipv4_hdr) + sizeof(struct rte_ether_hdr);
+
+						struct rte_mbuf *mbuf = rte_pktmbuf_alloc(mbuf_pool);
+						if (!mbuf) {
+							rte_exit(EXIT_FAILURE, "Error rte_pktmbuf_alloc\n");
+						}
+						mbuf->pkt_len = total_len;
+						mbuf->data_len = total_len;
+
+						uint8_t *msg = rte_pktmbuf_mtod(mbuf, uint8_t *);
+
+						ustack_encode_tcp_pkt(msg, total_len);
+
+						rte_eth_tx_burst(global_portid, 0, &mbuf, 1);
+
+						tcp_status = USTACK_TCP_STATUS_SYN_RCVD;
+					}
+				}
+
+				if (global_flags & RTE_TCP_ACK_FLAG) {
+
+
+					if (tcp_status == USTACK_TCP_STATUS_SYN_RCVD) {
+
+						printf("enter established\n");
+						tcp_status = USTACK_TCP_STATUS_ESTABLISHED;
+					}
+
+				}
+
+				if (global_flags & RTE_TCP_PSH_FLAG) {
+
+					printf("enter established: %d\n", tcp_status);
+					if (tcp_status == USTACK_TCP_STATUS_ESTABLISHED) {
+
+						uint8_t hdrlen = (tcphdr->data_off >> 4) * sizeof(uint32_t);
+
+						uint8_t *data = ((uint8_t*)tcphdr + hdrlen);
+
+						printf("tcp data: %s\n", data);
+					}
+
+				}
+			}
+        } 
 
 
     }
